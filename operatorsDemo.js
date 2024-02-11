7e395de95eabc975d2f20b9ccdb1c3cb3667ce77
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-11 19:56:25 +0530

    Updated operator demo

--- operatorsDemo.js
+++ operatorsDemo.js
@@ -24,3 +24,14 @@ console.log(a); // it means a = a / 2
 a**=2;
 console.log(a); // it means a = a**2
 
+// Relational Operators ( gives boolean as output)
+console.log(5>3); // greater than
+console.log(5<3); // less than
+console.log(5>=3); // greater than equal 
+console.log(5<=3); // less than equal
+
+// Logical Operators
+console.log(true && false); // logical AND 
+console.log(false || true); // Logical OR
+console.log(!true); // Logical NOT
+console.log((3>4) && (4>6)); 
