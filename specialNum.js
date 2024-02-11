9745aaab8f154c51f2cfa432401064319711af2c
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-11 20:43:50 +0530

    Added specialNum file

---
+++ specialNum.js
@@ -0,0 +1,11 @@
+// Special Numbers in js
+// 0
+console.log(0);
+console.log(-0);
+console.log(NaN);
+console.log(Infinity);
+console.log(-Infinity);
+
+// I can do comparison also
+
+console.log( 10 < Infinity ); // output = true
