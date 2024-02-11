f7929846dd585cb3c696bb805e3357f808bd3776
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-11 17:24:49 +0530

    Added console demo

---
+++ consolelogDemo.js
@@ -0,0 +1,15 @@
+// we'll see how can we display the given input(data) to the user as an output
+
+// there is an in-built function provided by js 
+// which is console.log()
+
+console.log("mohit");
+
+// different console.log() will print input with a newline
+
+let fruit = "apple";
+
+console.log(fruit); // fruit will get printed in newline
+
+// we can print multiple values in a single line by giving multiple inputs in a single log function
+console.log("Iphone14", 130000, "Red");
