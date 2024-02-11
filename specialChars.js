ac0bee9959d6bfa8de16c4eae948d76e42912ca1
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-11 17:26:16 +0530

    Added special chars demo

---
+++ specialChars.js
@@ -0,0 +1,19 @@
+// we have \n \t \\ etc special chars in js
+
+// \n is used for breaking a sentence with a newline
+
+let text = "Hello, Mr. Mohit, \nWhat are you doing today?";
+
+console.log(text);
+
+// \t is tab space 
+
+let text1 = "Beautiful life of my\tchildren";
+
+console.log(text1);
+
+// \\ use for printing backslash in a string
+
+let example = "We can sing\\dance.";
+
+console.log(example);
