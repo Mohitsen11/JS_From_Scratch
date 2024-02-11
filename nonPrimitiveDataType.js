ba630c01c0f7da3ea5ab3142e8cd6aed1bbded91
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-11 17:25:59 +0530

    Added object demo

---
+++ nonPrimitiveDataType.js
@@ -0,0 +1,15 @@
+// Object is one of non-primitive datatype
+
+let employee = {
+    name : "Alex",
+    company: "Amazon",
+    position: "Senior developer",
+    salary: "$400k",
+    employeeId: "Ax099"
+}
+
+// so rather than defining an real word entity data differently we use objects in place.
+
+console.log(employee);
+
+console.log(employee.company);
