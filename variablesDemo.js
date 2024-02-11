46b415a30b2fa10d1e9c344fc0c2d197f54d8a5b
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-11 16:41:06 +0530

    Added a file

---
+++ variablesDemo.js
@@ -0,0 +1,18 @@
+// We will see the that how can we store values in a bucket(variable) 
+
+// so there are three keyword available to store values 
+
+// first var
+
+var company = "Google";
+var age = 24;
+
+// second let
+let carName = "Honda";
+let price = 1000000;
+
+// third constant 
+const universityName = "SMVDU";
+const location = "Reasi";
+
+
