78e86dccdc38ad6a77522b69ee1590771ec942b0
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-11 19:33:50 +0530

    Added operator demo

---
+++ operatorsDemo.js
@@ -0,0 +1,26 @@
+// Arithmetic operators
+
+let x = 10;
+let y = 2;
+
+console.log(x+y); // addition
+console.log(x-y); // subtraction
+console.log(x*y); // multiplication
+console.log(x/y); // division
+console.log(x%y); // remainder
+console.log(y**2); // exponent
+
+// Assignment operators'
+
+let a = 10;
+a+=2;
+console.log(a); // it means a = a + 2 
+a-=2;
+console.log(a); // it means a = a - 2
+a*=2;
+console.log(a); // it means a = a * 2
+a/=2;
+console.log(a); // it means a = a / 2
+a**=2;
+console.log(a); // it means a = a**2
+
