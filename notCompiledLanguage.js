a9c9e1dafcfc77d1a668a8fc3b9a8506cd90fcea
Mohitsen11 <mohitsen0102@gmail.com>
2024-02-15 19:16:42 +0530

    Initial commit

---
+++ notCompiledLanguage.js
@@ -0,0 +1,6 @@
+// we will take an example which proves why it is not a compiled language
+
+console.log("hii");
+console.lo("Mohit"); // it will cause error we know that but 
+
+// hii will get printed which is not the intended behavior of the compiled based language.
---
+++ notPureInterpretedLanguage.js
@@ -0,0 +1,13 @@
+// Interpreted means which executes code line by line
+
+function fun1(){
+    let student = "Mohit";
+    console.log(student);
+}
+
+console.log("Mohit");
+// console...log("We are super cool!");
+console.log("byee");
+
+// if js is pure interpreted language then it must print Mohit before printing the error but it gives error first and doesn't print anything on the screen
+
